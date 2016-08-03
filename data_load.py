dd43930d52b2af48727614900f090159c4c8b73e
xingyou.chen <xingyou.chen@cootek.cn>
2016-08-03 10:11:47 +0800

    deta 1.0

--- data_load.py
+++ data_load.py
@@ -1,51 +1,71 @@
 import json
 import MySQLdb
+
+chart_date_need = {
+	"chart_user_count":['user_count'],
+	"chart_median_timeused":['median_timeused'],
+	"chart_avg_channel_time":['avg_channel_time'],
+	"chart_avg_carrier_match":['avg_carrier_match'],
+	"chart_history_match":['history_match'],
+	"chart_average_online":['average_online'],
+	"chart_avg_databyte":['avg_databyte'],
+	"chart_online_time":['average_online','online_fractile_5','online_fractile_20','online_fractile_50','online_fractile_80','online_fractile_95'],
+	"chart_data_byte":['avg_databyte','data_byte_fractile_50','data_byte_fractile_80','data_byte_fractile_95'],
+	"chart_online_section":['online_section_4h','online_section_8h','online_section_12h','online_section_16h','online_section_20h','online_section_24h'],
+	"chart_data_byte_section":['data_byte_section_100k','data_byte_section_1m','data_byte_section_10m','data_byte_section_100m']
+
+}
+
 class LL_data_loader(object):
 	def __init__(self):
 		self.conn=MySQLdb.connect("localhost","chenxingyou","123456","LL_data");
 		self.cursor = self.conn.cursor()	
 		self.str_select_date = " where log_date >= '%s' and log_date <= '%s';"
-		self.datalist = {'ios':[],'android':[]}
 
 	def __del__(self):
 		self.conn.close()
 		self.cursor.close()
 
-	def get_date_list(self, data_name_list, product_type ,phone_platform):
+	def get_date_list(self, chart_name, product_type ,phone_platform):
 		
-		self.datalist[phone_platform] =[]
-		
-		print data_name_list
-		for  one_data_kind in data_name_list:
-			str_select = "select " + one_data_kind + " from " + "LL_" + product_type + "_" + phone_platform  + self.str_select_date
-			self.cursor.execute( str_select % (self.date_dict["start_date"],self.date_dict["end_date"]))
-			org_one_data_list = self.cursor.fetchall()
-			one_data_list = []
-			for onedata in org_one_data_list:
-				one_data_list.append(onedata[0])
-			self.datalist[phone_platform] .append(one_data_list)
+	
 
-	def get_data_json(self, date_json, data_name_list, product_type,phone_platform_list):
-		print "data_json start"
+		print "get_date_list"
+		data_kind =" " 
+		
+		for  one_data_kind in chart_date_need[chart_name]:
+			data_kind += one_data_kind + ','
+
+		data_kind = data_kind[:-1]
+		
+		str_select = "select " + data_kind + " from " + "LL_" + product_type + "_" + phone_platform  + self.str_select_date
+		print str_select % (self.date_dict["start_date"],self.date_dict["end_date"])
+		self.cursor.execute( str_select % (self.date_dict["start_date"],self.date_dict["end_date"]))
+		org_one_data_list = self.cursor.fetchall()
+		
+		
+		one_chart_data = {}
+		num = 0
+		for  one_data_kind in chart_date_need[chart_name]:
+			one_chart_data[one_data_kind] = [x[num] for x in org_one_data_list]
+			num += 1
+		
+		return one_chart_data
+
+	def get_data_json(self, date_json, charts_name, product_type,phone_platform_list):
+		
+		print 1
 		self.date_dict = json.loads(date_json)
 
-		for  phone_platform in phone_platform_list:
-			self.get_date_list(data_name_list, product_type ,phone_platform)
-		
 		data_dict = self.date_dict.copy()
-
-		if 'ios' not in phone_platform_list:
-			data_dict['ios_data'] = None
-		else:
-			data_dict['ios_data'] = self.datalist['ios']
-
-		if 'android' not in phone_platform_list:
-			data_dict['android_data'] = None
-		else:
-			data_dict['android_data'] = self.datalist['android']
+		data_dict['ios']={}
+		data_dict['android']={}
+		for  one_chart_name in charts_name:
+			for  phone_platform in phone_platform_list:
+				data_dict[phone_platform][one_chart_name] = self.get_date_list(one_chart_name, product_type ,phone_platform)
 
 		data_json = json.dumps(data_dict)
-		print "data_json ok"
+		print data_json
 		return data_json
 
 		
---
+++ static/echarts/zhexian.js
@@ -0,0 +1,274 @@
+var date=[];
+var ios_data=[];
+var android_data=[];
+var Charts_one = {};
+var Charts_mix = {};
+
+var charts_name_format = {
+	"chart_user_count":"用户数量",
+	"chart_median_timeused":'平均呼叫时长',
+	"chart_avg_channel_time":'平均延时',
+	"chart_avg_carrier_match":'最优连接占比',
+	"chart_history_match":'依赖于历史记录的最优连接占比',
+	"chart_average_online":'平均在线时长',
+	"chart_avg_databyte":'平均流量消耗',
+	"chart_online_time":'平均在线时长分位图',
+	"chart_data_byte":'平均流量消耗分位图'
+}
+var chart_one_data_need = {
+	"chart_user_count":'user_count',
+	"chart_median_timeused":'median_timeused',
+	"chart_avg_channel_time":'avg_channel_time',
+	"chart_avg_carrier_match":'avg_carrier_match',
+	"chart_history_match":'history_match',
+	"chart_average_online":'average_online',
+	"chart_avg_databyte":'avg_databyte',
+}
+
+var chart_mix_data_need = {
+	"chart_online_time":['平均在线时长','5分位','20分位','50分位','80分位','95分位'],
+	"chart_online_time_EN":['average_online','online_fractile_5','online_fractile_20','online_fractile_50','online_fractile_80','online_fractile_95'],
+	"chart_data_byte":['平均流量消耗','50分位','80分位','95分位'],
+	"chart_data_byte_EN":['avg_databyte','data_byte_fractile_50','data_byte_fractile_80','data_byte_fractile_95']
+}
+
+
+function create_zhexian_one(div_id, chart_name, product_name, platform){
+	
+	var new_myChart= echarts.init(document.getElementById(div_id),'macarons');
+	Charts_one[chart_name] = new_myChart;
+	new_myChart.setOption({
+	    title : {
+		text: charts_name_format[chart_name],
+		subtext: '纯属虚构'
+	    },
+	    tooltip : {
+		trigger: 'axis'
+	    },
+	    legend: {
+		data:['ios','android']
+		},
+		toolbox: {
+		show : true,
+		feature : {
+		    mark : {show: true},
+		    dataView : {show: true, readOnly: false},
+		    magicType : {show: true, type: ['line', 'bar']},
+		    restore : {show: true},
+		    saveAsImage : {show: true}
+		}
+	    },
+	    calculable : true,
+	    xAxis : [{
+		type : 'category',
+		data : date
+	    }],
+	    yAxis : [
+	    {
+		type : 'value',
+		scale:true,
+		axisLabel : {formatter:'{value} '}
+	    }
+	    ],
+	    series : [
+	    {
+		name:'ios',
+		type:'line',
+		data:ios_data,
+		 markPoint : {
+		    data : [
+		        {type : 'max', name: '最大值'},
+		        {type : 'min', name: '最小值'}
+		    ]
+		},
+		markLine : {
+		    data : [
+		        {type : 'average', name: '平均值'}
+		    ]
+		},
+		itemStyle : {
+          		normal : {
+          			lineStyle : {
+          				width : 2
+          				
+       				 }
+       			 }
+        	}
+	    },
+	    {
+		name:'android',
+		type:'line',
+		data:android_data,
+		markPoint : {
+		    data : [
+		        {type : 'max', name: '最大值'},
+		        {type : 'min', name: '最小值'}
+		    ]
+		},
+		markLine : {
+		    data : [
+		        {type : 'average', name : '平均值'}
+		    ]
+		},
+		itemStyle : {
+          		normal : {
+          			lineStyle : {
+          				width : 2
+          				
+       				 }
+       			 }
+        	}
+	    }
+	    ]
+	});
+
+}
+
+function create_zhexian_mix(div_id, chart_name, product_name, platform){
+	var new_myChart= echarts.init(document.getElementById(div_id),'macarons');
+	Charts_mix[platform + '_' + chart_name ] = new_myChart;
+	
+	new_myChart.setOption({
+	    title : {
+		text: platform + charts_name_format[chart_name],
+		subtext: '纯属虚构'
+	    },
+	    tooltip : {
+		trigger: 'axis'
+	    },
+	    legend: {
+		data:chart_mix_data_need[chart_name]
+		},
+		toolbox: {
+		show : true,
+		feature : {
+		    mark : {show: true},
+		    dataView : {show: true, readOnly: false},
+		    magicType : {show: true, type: ['line', 'bar']},
+		    restore : {show: true},
+		    saveAsImage : {show: true}
+		}
+	    },
+	    calculable : true,
+	    xAxis : [{
+		type : 'category',
+		data : date
+	    }],
+	    yAxis : [
+	    {
+		type : 'value',
+		axisLabel : {formatter:'{value} '}
+	    }
+	    ],
+	    series :function(){
+		var serie=[];
+		for( var i=0;i < chart_mix_data_need[chart_name].length;i++){
+			var item={
+			name:chart_mix_data_need[chart_name][i],
+			data:[],
+			type:'line',
+			
+			}
+			serie.push(item);
+		};
+		return serie;
+		}()
+	});
+
+}
+
+
+function get_date_list(start_date, end_date){
+	start_date = new Date(start_date.replace(/-/g, "/"));
+	end_date = new Date(end_date.replace(/-/g,  "/"));
+	new_date = [];
+	for(var day = start_date;day <= end_date; day.setDate( day.getDate() + 1)){
+		new_date.push(day.toLocaleDateString());
+	}
+	return new_date;
+}
+
+function update_data(org_data_json){
+	eval('var org_data ='+org_data_json);
+	ios_data = org_data['ios'];
+	android_data = org_data['android'];
+	date = get_date_list(org_data['start_date'], org_data['end_date']);	
+	for(var chart_name in Charts_one){
+		Charts_one[chart_name].setOption({
+			xAxis : [{
+				type : 'category',
+				data : date
+		    	}],
+			series: [
+			{
+				name:'ios',
+				data:ios_data[chart_name][chart_one_data_need[chart_name]],
+				type:'line'
+			},
+			{
+				name:'android',
+				data:android_data[chart_name][chart_one_data_need[chart_name]],
+				type:'line'
+			}]
+		});
+	}
+	for(var chart_name in Charts_mix){
+		
+		
+		var mix_data;
+		var real_name;
+		if(chart_name[0] == 'i'){
+			mix_data = ios_data;
+			real_name = chart_name.substr(4);
+		}
+		else{
+			mix_data = android_data;
+			real_name = chart_name.substr(8);
+		}
+		
+		Charts_mix[chart_name].setOption({
+			xAxis : [{
+				type : 'category',
+				data : date
+		    	}],
+			series: function(){
+				var serie=[];
+				for( var i=0;i < chart_mix_data_need[real_name].length;i++){
+					
+					if(chart_mix_data_need[real_name+'_EN'][i].indexOf('avg') >=0 || chart_mix_data_need[real_name+'_EN'][i].indexOf('average') >= 0){
+						var item={
+						name:chart_mix_data_need[real_name][i],
+						data:mix_data[real_name][chart_mix_data_need[real_name+'_EN'][i]],
+						type:'line',
+						itemStyle: {
+							normal: {
+							    color: 'red',
+							    lineStyle: {       
+								width: 2,
+								type: 'dashed'
+							    }
+							}
+						}
+						}
+						serie.push(item);
+						
+					}
+					else{
+						var item={
+						name:chart_mix_data_need[real_name][i],
+						data:mix_data[real_name][chart_mix_data_need[real_name+'_EN'][i]],
+						type:'line',
+						itemStyle: {normal: {areaStyle: {type: 'default'}}},
+						}
+						serie.push(item);
+					}
+				};
+				return serie;
+				}()
+		});
+	}
+
+	
+}
+
+
